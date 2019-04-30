6667f5c5bbf828ab05b002d30c8a1bf43068f5aa
deathbykender <45900750+abferris@users.noreply.github.com>
2019-04-29 23:09:58 -0700

    Grid designs
    Grid designs are all done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import Header from "./components/header/header.js";
 import Footer from "./components/footer/footer.js";
 import Content from "./components/content/content.js";
 import List from "./components/list/list.js"
+import Grid from "./components/grid/grid.js"
 // import "./design/reset.scss";
 import "./design/design.scss";
 import "./design/formparts/form.scss"
@@ -12,9 +13,7 @@ const App = () => {
     <>
       <Header title="React Design" />
       <Content />
-      <Footer>
-        <div>&copy; 2018 Code Fellows</div>
-      </Footer>
+ 
       <List 
       type='ordered'
       items={['a', 'b', 'c']}
@@ -23,6 +22,13 @@ const App = () => {
       type='unordered'
       items={['do', 're', 'mi']}
       />
+      <Grid 
+      elements={['Time','space','everything','my life']}
+      columns={'four'}
+      />
+      <Footer>
+        <div>&copy; 2018 Code Fellows</div>
+      </Footer>
     </>
   );
 };
